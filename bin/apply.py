a1a7bd56960f4a780ef521051a414822c1794165
Christian Archer <sunchaser@sunchaser.info>
2023-03-13 00:58:14 +0200

    Process subdirs in apply

--- bin/apply.py
+++ bin/apply.py
@@ -4,25 +4,45 @@ import os
 import shlex
 import sys
 
+
+def process(src_dir, dst_dir, meta_dir):
+    for file in os.listdir(meta_dir):
+        meta_path = meta_dir + '/' + file
+
+        if os.path.isdir(meta_path):
+            process(src_dir + '/' + file, dst_dir + '/' + file, meta_path)
+            continue
+
+        if not file.endswith('.txt'):
+            continue
+
+        fn_src = file[:-4]
+        if fn_src.endswith('.avi'):
+            fn_dst = fn_src[:-4] + '.mp4'
+        else:
+            fn_dst = fn_src
+        src_file = src_dir + '/' + fn_src
+        src_file_q = shlex.quote(src_dir + '/' + fn_src)
+        dst_file_q = shlex.quote(dst_dir + '/' + fn_dst)
+        meta_file_q = shlex.quote(meta_path)
+
+        if not os.path.exists(dst_dir):
+            os.makedirs(dst_dir, exist_ok=True)
+
+        if not os.path.exists(src_file):
+            print(f'Skipping missing {fn_src}')
+            continue
+
+        cmd = f'ffmpeg -y -i {src_file_q} -i {meta_file_q} -map_metadata 1 -codec copy {dst_file_q}'
+
+        print(f'>>> {cmd}')
+        os.system(cmd)
+
+
 src_dir = sys.argv[1]
 dst_dir = sys.argv[2]
 
 proj_dir = os.path.dirname(sys.argv[0]) + '/..'
 meta_dir = proj_dir + '/ffmeta'
 
-for file in os.listdir(meta_dir):
-    if not file.endswith('.txt'):
-        break
-
-    fn_src = file[:-4]
-    if fn_src.endswith('.avi'):
-        fn_dst = fn_src[:-4] + '.mp4'
-    else:
-        fn_dst = fn_src
-    src_file = shlex.quote(src_dir + '/' + fn_src)
-    dst_file = shlex.quote(dst_dir + '/' + fn_dst)
-    meta_file = shlex.quote(meta_dir + '/' + file)
-    cmd = f'ffmpeg -y -i {src_file} -i {meta_file} -map_metadata 1 -codec copy {dst_file}'
-
-    print(f'>>> {cmd}')
-    os.system(cmd)
+process(src_dir, dst_dir, meta_dir)
